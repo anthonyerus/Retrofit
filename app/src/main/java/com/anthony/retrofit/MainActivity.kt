304cce9e4f7e6433c230c8640a4caa21d0d80ec1
anthonyerus <85246479+anthonyerus@users.noreply.github.com>
2021-08-12 21:49:06 +0100

    coroutines

--- app/src/main/java/com/anthony/retrofit/MainActivity.kt
+++ app/src/main/java/com/anthony/retrofit/MainActivity.kt
@@ -2,14 +2,20 @@ package com.anthony.retrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.anthony.retrofit.databinding.ActivityMainBinding
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import io.github.retrofit.databinding.ActivityMainBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var todoAdapter: TodoAdapter
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,23 +26,14 @@ class MainActivity : AppCompatActivity() {
         todoAdapter = TodoAdapter(listOf())
         binding.recycView.adapter = todoAdapter
 
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
-
-        val todos: Call<List<Todo>> = RetrofitProvider.service.getAllTodos()
-
-        todos.enqueue(object : Callback<List<Todo>> {
-            override fun onResponse(call: Call<List<Todo>>, response: Response<List<Todo>>) {
-                if (response.isSuccessful){
-                    response.body()?.let { todos ->
-                        todoAdapter.list = todos
-                        todoAdapter.notifyDataSetChanged()
-                    }
-                }
+        viewModel.apply {
+            getAllTodos()
+            items.observe(this@MainActivity){ todos ->
+                todoAdapter.list = todos
+                todoAdapter.notifyDataSetChanged()
             }
-
-            override fun onFailure(call: Call<List<Todo>>, t: Throwable) {
-            }
-
-        })
+        }
     }
 }
